d6dc08f704ee10d47fec0a9ac8d3d0bf4988f925
johnjhacking <39013067+johnjhacking@users.noreply.github.com>
2024-05-08 13:07:53 -0600

    Create gen_def.py

---
+++ gen_def.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Credit to Tothi
+
+import pefile
+import sys
+import os.path
+
+dll = pefile.PE(sys.argv[1])
+dll_basename = os.path.splitext(sys.argv[1])[0]
+
+print("EXPORTS")
+for export in dll.DIRECTORY_ENTRY_EXPORT.symbols:
+    if export.name:
+        print('{}={}.{} @{}'.format(export.name.decode(), dll_basename, export.name.decode(), export.ordinal))
